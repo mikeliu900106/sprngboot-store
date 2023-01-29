bc801e90e42b5b19b9d905aecffcccaac6b5bef2
mikeliu900106 <mikeliu20010106@gmail.com>
2023-01-29 17:24:28 +0800

    "first commit"

---
+++ sprngboot-store/src/main/java/com/example/sprngbootstore/SprngbootStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprngbootstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprngbootStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprngbootStoreApplication.class, args);
+	}
+
+}
---
+++ sprngboot-store/src/test/java/com/example/sprngbootstore/SprngbootStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprngbootstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprngbootStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
